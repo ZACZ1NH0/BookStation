5d3b78598d853a9eeaa9cc7bb17e0eb0f6541bd4
TaTuan-no1 <tqt060404@gmail.com>
2025-05-27 17:11:37 +0700

    Fix structure and update forms

--- BookStation/orders/views.py
+++ BookStation/orders/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from decimal import Decimal
 from .models import Order, OrderItem
-from books.models import Book  
+from books.models import Book
 from .forms import OrderForm, OrderItemFormSet
 
 
