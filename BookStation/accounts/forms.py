8410fd3f231a838db8b9307062abafedd5e99044
TaTuan-no1 <tqt060404@gmail.com>
2025-05-27 16:36:05 +0700

    a

--- BookStation/accounts/forms.py
+++ BookStation/accounts/forms.py
@@ -1,7 +1,14 @@
 from django.contrib.auth.forms import UserCreationForm
 from .models import Users
+from django import forms
+
 
 class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = Users
+        fields = ('username', 'email', 'phone','first_name','last_name' )
+
+class EditProfile(forms.ModelForm):
     class Meta:
         model = Users
         fields = ('username', 'email', 'phone','first_name','last_name' )
