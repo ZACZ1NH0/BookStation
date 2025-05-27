b8f6187b0420fb03ea8ff252d3d5bea697cda220
ZACZ1NH0 <nguyenlongvu768@gmail.com>
2025-05-27 16:49:19 +0700

    fix

--- BookStation/orders/models.py
+++ BookStation/orders/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from .models import Book
+from books.models import Book
 
 class Order(models.Model):
     STATUS_CHOICES = [
