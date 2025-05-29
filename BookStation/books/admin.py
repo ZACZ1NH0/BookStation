f2b5e61c2858f5f407281f98d3c51f374a0f9080
hanchivu3003 <hanchivu3003@gmail.com>
2025-05-29 14:48:44 +0700

    update cart, css, image,...

--- BookStation/books/admin.py
+++ BookStation/books/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Book
+from .models import Author, Publisher, Category
+admin.site.register(Author) 
+admin.site.register(Book)
+admin.site.register(Publisher)
+admin.site.register(Category)
