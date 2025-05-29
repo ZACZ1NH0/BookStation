3a752fc396aefd4d3f58e43e19504bdcb58706a7
ZACZ1NH0 <nguyenlongvu768@gmail.com>
2025-05-30 06:27:27 +0700

    Register

--- BookStation/BookStation/urls.py
+++ BookStation/BookStation/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('books/', include('books.urls')),
     path('orders/', include('orders.urls'))
-] 
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
