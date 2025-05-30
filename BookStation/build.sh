31d5ff8b4831c0d9f0578a533e9e18dc0f9c8756
ZACZ1NH0 <nguyenlongvu768@gmail.com>
2025-05-30 13:52:13 +0700

    add build and render

---
+++ BookStation/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Cài thư viện
+pip install -r requirements.txt
+
+# Chạy migrate
+python manage.py migrate
+
+# Tạo superuser nếu chưa có
+echo "
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'soloyasuokom@gmail.com', '1234')
+    print('Superuser created.')
+else:
+    print('Superuser already exists.')
+" | python manage.py shell
