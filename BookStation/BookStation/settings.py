074b869c13a623fc3029b190a72f9c6903aed2c2
ZACZ1NH0 <nguyenlongvu768@gmail.com>
2025-05-30 12:21:19 +0700

    sua setting

--- BookStation/BookStation/settings.py
+++ BookStation/BookStation/settings.py
@@ -150,3 +150,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+CSRF_TRUSTED_ORIGINS = ['https://bookstation-vjjf.onrender.com']
