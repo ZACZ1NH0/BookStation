7da27257b6d38f8b1d1134e5f1aa6026dbd26d1e
ZACZ1NH0 <nguyenlongvu768@gmail.com>
2025-06-09 01:29:34 +0700

    fix hien thi review

--- BookStation/reviews/models.py
+++ BookStation/reviews/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 from books.models import Book
 
-class Book(models.Model):
-    title = models.CharField(max_length=200)
-    description = models.TextField()
+# class Book(models.Model):
+#     title = models.CharField(max_length=200)
+#     description = models.TextField()
 
 class Review(models.Model):
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
